a40aabdf9181ecf2ec125fa9b446d12150f9a85a
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-05-14 01:31:03 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,58 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() {
+  first = nullptr;
+  countOp = 0;
+}
+
+void Train::addCage(bool light) {
+  if (first == nullptr) {
+    first = new Cage();
+    first -> light = lignt;
+    first -> next = first;
+    first -> prev = first;
+  } else {
+    Cage* nextCage = new Cage();
+    nextCage -> light = light;
+    nextCage -> prev = first -> prev;
+    nextCage -> next = first;
+    first -> prev -> next = nextCage;
+    first -> prev = nextCage;
+  }
+}
+
+int Train::getLength() {
+  int count = 1;
+  Cage* cur = first;
+  cur -> light = true;
+  while (cur) {
+    cur = cur -> next;
+    checkF(cur, count);
+    if (cur -> light == true) {
+      cur -> light = false;
+      ++countOp;
+      for (int i = count; i > 0; --i) {
+        cur = cur -> prev;
+        ++countOp;
+      }
+      if (cur->light == false) {
+        return count;
+			}
+			else {
+        count = 1;
+			}
+    }
+  }
+}
+
+int Train::getOpCount() {
+	return countOp;
+}
+
+
+void Train::checkF(Cage* cur, int& count) {
+	if (cur->light == false) {
+		++count;
+		++countOp;
+	}
+}
