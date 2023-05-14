8d6aa199ee9c2c91d6c53d293f8610559bda4a6d
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-05-14 07:20:10 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,18 +1,18 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 Train::Train() {
-    first = nullptr;
-    countOp = 0;
+  first = nullptr;
+  countOp = 0;
 }
 
 void Train::addCage(bool light) {
-	if (first == nullptr) {
-		first = new Cage();
-		first -> light = light;
+  if (first == nullptr) {
+    first = new Cage();
+    first -> light = light;
 		first -> next = first;
 		first -> prev = first;
    } else {
-		Cage* nextCage = new Cage();
+    Cage* nextCage = new Cage();
 		nextCage -> light = light;
 		nextCage -> prev = first -> prev;
 		nextCage -> next = first;
@@ -22,38 +22,38 @@ void Train::addCage(bool light) {
 }
 
 int Train::getLength() {
-	int count = 1;
-	Cage* cur = first;
-    cur -> light = true;
-	while (cur) {
-		cur = cur -> next;
-        checkF(cur, count);
-        if (cur -> light == true) {
-            cur -> light = false;
-            ++countOp;
-            for (int i = count; i > 0; --i) {
-                cur = cur -> prev;
-                ++countOp;
-            }
-            if (cur -> light == false) {
-                return count;
-            } else {
-                count = 1;
-            }
-        }
+  int count = 1;
+  Cage* cur = first;
+  cur -> light = true;
+  while (cur) {
+    cur = cur -> next;
+    checkF(cur, count);
+    if (cur -> light == true) {
+      cur -> light = false;
+      ++countOp;
+      for (int i = count; i > 0; --i) {
+        cur = cur -> prev;
+        ++countOp;
+      }
+      if (cur -> light == false) {
+        return count;
+      } else {
+        count = 1;
+      }
     }
-	return 0;
+  }
+  return 0;
 }
 
 
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
 }
 
 
 void Train::checkF(Cage* cur, int& count) {
-	if (cur->light == false) {
-		++count;
-		++countOp;
+  if (cur->light == false) {
+    ++count;
+    ++countOp;
 	}
 }
